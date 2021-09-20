3ffb8345ae4069be28738af7c9397660917ac889
Rachel Trana <retrana@neiu.edu>
2021-09-19 21:13:44 -0500

    fixing autograder again

--- tests/test_problem1.py
+++ tests/test_problem1.py
@@ -1,21 +1,21 @@
+from unittest import TestCase
 # from problem1.problem1 import is_even
 
 
-def test_is_even():
-    pass
-    # assert is_even(32)
+class TestProblem(TestCase):
 
+    def test_is_even(self):
+        pass
+        # self.assertTrue(is_even(32))
 
-def test_is_even2():
-    pass
-    # assert is_even(-24)
+    def test_is_even2(self):
+        pass
+        # self.assertTrue(is_even(-24))
 
+    def test_is_not_even(self):
+        pass
+        # self.assertFalse(is_even(59))
 
-def test_is_not_even():
-    pass
-    # assert not is_even(59)
-
-
-def test_is_not_even2():
-    pass
-    # assert not is_even(-31)
+    def test_is_not_even2(self):
+        pass
+        # self.assertFalse(is_even(-31))
